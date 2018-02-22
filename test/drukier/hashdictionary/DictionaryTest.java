6dd4768d651f0abf5598cb7963953e779e16f0c0
Chana Drukier <chana@Chanas-MacBook-Pro.local>
2018-02-22 17:43:31 -0500

    Test

---
+++ test/drukier/hashdictionary/DictionaryTest.java
@@ -0,0 +1,24 @@
+package drukier.hashdictionary;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+
+public class DictionaryTest {
+
+	@Test
+	public void testDefine() throws FileNotFoundException{
+		// given
+		HashDictionary D = new HashDictionary("/Users/chana/Documents/Touro/Spring 2018/Computer Methodology/VendingMachine/dictionary.txt");
+		
+		// when
+			String word = "apple";
+			String definition =  "apple:  an edible fruit [n -S]";
+
+		// then
+		assertEquals(definition, D.define(word));
+	}
+
+}
