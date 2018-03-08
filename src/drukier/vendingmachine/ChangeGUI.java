cfdf968199489ba3a3cfc999471fbff29f7c037d
Chana Drukier <chana@Chanas-MBP.home>
2018-03-08 13:33:40 -0500

    Change GUI fix

--- src/drukier/vendingmachine/ChangeGUI.java
+++ src/drukier/vendingmachine/ChangeGUI.java
@@ -15,18 +15,19 @@ public class ChangeGUI extends JFrame {
 	JButton makeChange = new JButton("Make Change");
 
 	public ChangeGUI() {
-		setTitle("Change Maker ");
-		setSize(800, 600);
+		setTitle("Vending Machine");
+		setSize(300, 600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+		
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(0, 2));
 
-		panel.add(new JLabel("Price: ", SwingConstants.RIGHT));
+		panel.add(new JLabel("Price:\t\t\t\t$", SwingConstants.RIGHT));
 		panel.add(price);
-		panel.add(new JLabel("Paid: ", SwingConstants.RIGHT));
+		panel.add(new JLabel("Paid:\t\t\t\t$", SwingConstants.RIGHT));
 		panel.add(paid);
-		panel.add(new JLabel("Change: ", SwingConstants.RIGHT));
+		panel.add(new JLabel("Pay! ", SwingConstants.RIGHT));
 		panel.add(makeChange);
 		panel.add(new JLabel("Quarters: ", SwingConstants.RIGHT));
 		panel.add(quarters);
@@ -36,19 +37,21 @@ public class ChangeGUI extends JFrame {
 		panel.add(nickels);
 		panel.add(new JLabel("Pennies: ", SwingConstants.RIGHT));
 		panel.add(pennies);
-
-		JButton button = new JButton("Button");
-		button.addActionListener(this::makeChange);
-
+		
+		//JButton button = new JButton("Button");
+		makeChange.addActionListener(this::makeChange);
+		
+		add(panel);
 	}
 
 	public void makeChange(ActionEvent event) {
-		Change change = VendingMachine.pay(Double.parseDouble(price.toString()),Double.parseDouble(paid.toString()));
+		Change change = VendingMachine.pay(Double.parseDouble(price.getText()), Double.parseDouble(paid.getText()));
+		//change.makeChange(Double.parseDouble(price.getText()) - Double.parseDouble(paid.getText()));
 		quarters.setText(Integer.toString(change.getQuarters()));
 		dimes.setText(Integer.toString(change.getDimes()));
 		nickels.setText(Integer.toString(change.getNickels()));
 		pennies.setText(Integer.toString(change.getPennies()));
-		
+
 	}
 
 	public static void main(String[] args) {
