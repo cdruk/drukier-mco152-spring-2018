2042daaca8ebc4d7ec228c8e1d8fa33539ce086e
Chana Drukier <chana@Chanas-MacBook-Pro.local>
2018-03-08 17:53:12 -0500

    fixed projectile GUI

--- src/drukier/physics/ProjectileGui.java
+++ src/drukier/physics/ProjectileGui.java
@@ -8,24 +8,22 @@ import java.beans.PropertyChangeEvent;
 
 import javax.swing.*;
 
-public class ProjectileGui extends JFrame
-{
-	
-	JTextField angle = new JTextField("");
-	JTextField velocity = new JTextField("");
-	JTextField time = new JTextField("");
-	JTextField x = new JTextField();
-	JTextField y = new JTextField();
-	
-	public ProjectileGui()
-	{
+public class ProjectileGui extends JFrame {
+
+	JFormattedTextField angle = new JFormattedTextField("0");
+	JFormattedTextField velocity = new JFormattedTextField("0");
+	JFormattedTextField time = new JFormattedTextField("0");
+	JFormattedTextField x = new JFormattedTextField();
+	JFormattedTextField y = new JFormattedTextField();
+
+	public ProjectileGui() {
 		setTitle("Projectile Viewer");
-		setSize(800,600);
+		setSize(800, 600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(6,0));
-		
+		panel.setLayout(new GridLayout(6, 0));
+
 		panel.add(new JLabel("Angle:", SwingConstants.CENTER));
 		panel.add(angle);
 		panel.add(new JLabel("Velocity:", SwingConstants.CENTER));
@@ -38,29 +36,29 @@ public class ProjectileGui extends JFrame
 		panel.add(x);
 		panel.add(new JLabel("Y: ", SwingConstants.CENTER));
 		panel.add(y);
-		
-		
-		angle.addPropertyChangeListener("angle", this::findCoordinates);
-		velocity.addPropertyChangeListener("velocity", this::findCoordinates);
-		time.addPropertyChangeListener("time", this::findCoordinates);
-		
-		
+
+		angle.addPropertyChangeListener("value", this::findCoordinates);
+		velocity.addPropertyChangeListener("value", this::findCoordinates);
+		time.addPropertyChangeListener("value", this::findCoordinates);
+
 		add(panel);
-		
+
 	}
-	
+
 	public void findCoordinates(PropertyChangeEvent event) {
+
 		double thisAngle = Double.parseDouble(angle.getText());
 		double thisVelocity = Double.parseDouble(velocity.getText());
 		double thisTime = Double.parseDouble(time.getText());
-		Projectile projectile = new Projectile(thisAngle, thisVelocity);
-		x.setText(Double.toString(projectile.findX(thisTime)));
-		y.setText(Double.toString(projectile.findY(thisTime)));
-	}
+
 		
-	
-	public static void main(String[] args)
-	{
+			Projectile projectile = new Projectile(thisAngle, thisVelocity);
+			x.setValue(Double.toString(projectile.findX(thisTime)));
+			y.setValue(Double.toString(projectile.findY(thisTime)));
+		
+	}
+
+	public static void main(String[] args) {
 		new ProjectileGui().setVisible(true);
 	}
 }
