66a3a46d9617a9658de3899d8f70981ad99842cd
cdruk <cdrukier@student.touro.edu>
2018-03-22 23:50:56 -0400

    projectile hw

--- src/drukier/physics/ProjectileGraph.java
+++ src/drukier/physics/ProjectileGraph.java
@@ -11,22 +11,47 @@ public class ProjectileGraph extends JComponent {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		// g.setColor(Color.BLACK);
-		// g.fillRect(0, 0, 600, 6\00);
-		//
-		// g.setColor(Color.RED);
-		// g.drawLine(10, 50, 140, 140);
-		// g.drawOval(14, 14, 400, 400);
-
-		Projectile projectile = new Projectile(54, 108);
-		
-		g.setColor(Color.RED);
-		g.translate(0, getHeight());
-		for (int time = 0; time <= 10; time++) {
-			g.fillOval((int) projectile.getX(time), (int) -projectile.getY(time), 5, 5);
-			g.drawLine((int) projectile.getX(time), (int) -projectile.getY(time), 1, 2);
-			g.drawString("(" + projectile.getX(time) + "), (" + projectile.getY(time) + ")" , (int) projectile.getX(time), (int) -projectile.getY(time));
+		g.setColor(Color.LIGHT_GRAY);
+		for (int increment = 0; increment <= getHeight(); increment = increment + 10) {
+			g.drawLine(0, increment, getWidth(), increment);
+		}
+		for (int increment = 0; increment <= getWidth(); increment = increment + 10) {
+			g.drawLine(increment, 0, increment, getHeight());
 		}
 
+		g.translate(0, getHeight());
+
+		Projectile projectile1 = new Projectile(54, 108);
+
+		g.setColor(Color.RED);
+		for (int time = 0; time <= 10; time++) {
+			g.fillOval((int) projectile1.getX(time), (int) -projectile1.getY(time), 5, 5);
+			g.drawLine((int) projectile1.getX(time), (int) -projectile1.getY(time), (int) projectile1.getX(time - 1),
+					(int) -projectile1.getY(time - 1));
+			g.drawString("(" + projectile1.getX(time) + "), (" + projectile1.getY(time) + ")",
+					(int) projectile1.getX(time), (int) -projectile1.getY(time));
+		}
+
+		Projectile projectile2 = new Projectile(63, 87);
+
+		g.setColor(Color.BLUE);
+		for (int time = 0; time <= 10; time++) {
+			g.fillOval((int) projectile2.getX(time), (int) -projectile2.getY(time), 5, 5);
+			g.drawLine((int) projectile2.getX(time), (int) -projectile2.getY(time), (int) projectile2.getX(time - 1),
+					(int) -projectile2.getY(time - 1));
+			g.drawString("(" + projectile2.getX(time) + "), (" + projectile2.getY(time) + ")",
+					(int) projectile2.getX(time), (int) -projectile2.getY(time));
+		}
+
+		Projectile projectile3 = new Projectile(42, 279);
+
+		g.setColor(Color.GREEN);
+		for (int time = 0; time <= 10; time++) {
+			g.fillOval((int) projectile3.getX(time), (int) -projectile3.getY(time), 5, 5);
+			g.drawLine((int) projectile3.getX(time), (int) -projectile3.getY(time), (int) projectile3.getX(time - 1),
+					(int) -projectile3.getY(time - 1));
+			g.drawString("(" + projectile3.getX(time) + "), (" + projectile3.getY(time) + ")",
+					(int) projectile3.getX(time), (int) -projectile3.getY(time));
+		}
 	}
 }
