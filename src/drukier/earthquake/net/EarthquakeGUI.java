546d19b558f1db2dc8420a9ba25e4149a711909c
cdruk <cdrukier@student.touro.edu>
2018-04-26 23:59:20 +0200

    earthquake update

--- src/drukier/earthquake/net/EarthquakeGUI.java
+++ src/drukier/earthquake/net/EarthquakeGUI.java
@@ -10,57 +10,51 @@ import javax.swing.*;
 
 public class EarthquakeGUI extends JFrame {
 
-	private String hMag;
-	private String hPlace;
-	private String dMag;
-	private String dPlace;
-	private String wMag;
-	private String wPlace;
-	private String mMag;
-	private String mPlace;
-	private JButton getValues = new JButton("Get Largest Earthquake Values");
 
-	public EarthquakeGUI() {
+	public EarthquakeGUI(EarthquakeRetrofitClient earthquake) {
 		setTitle("Largest Earthquake");
 		setSize(250, 400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		//mMag = earthquake.getmMag();
-		//mPlace = earthquake.getmPlace();
-		JTextField mLargest = new JTextField(mMag + ": " + mPlace);
-
+		JTextField hMag = new JTextField (earthquake.gethMag());
+		JTextField hPlace = new JTextField (earthquake.gethPlace());
+		JTextField dMag = new JTextField (earthquake.getdMag());
+		JTextField dPlace = new JTextField (earthquake.getdPlace());
+		JTextField wMag = new JTextField (earthquake.getwMag());
+		JTextField wPlace = new JTextField (earthquake.getwPlace());
+		JTextField mMag = new JTextField (earthquake.getmMag());
+		JTextField mPlace = new JTextField (earthquake.getmPlace());
+		
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 
 		panel.add(new JLabel("Largest Earthquake"), BorderLayout.NORTH);
 
 		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new GridLayout(0, 2));
+		mainPanel.setLayout(new GridLayout(0, 3));
 		mainPanel.add(new JLabel("Last Hour", SwingConstants.CENTER));
-		// mainPanel.add((hMag + ": " + hPlace), SwingConstants.CENTER);
+		mainPanel.add(hMag, SwingConstants.CENTER);
+		mainPanel.add(hPlace, SwingConstants.CENTER);
 		mainPanel.add(new JLabel("Last Day", SwingConstants.CENTER));
-		// mainPanel.add(dMag, dPlace);
+		mainPanel.add(dMag, SwingConstants.CENTER);
+		mainPanel.add(dPlace, SwingConstants.CENTER);
 		mainPanel.add(new JLabel("Last Week", SwingConstants.CENTER));
-		// mainPanel.add(wMag, wPlace);
+		mainPanel.add(wMag, SwingConstants.CENTER);
+		mainPanel.add(wPlace, SwingConstants.CENTER);
 		mainPanel.add(new JLabel("Last Month", SwingConstants.CENTER));
-		mainPanel.add(mLargest, SwingConstants.CENTER);
+		mainPanel.add(mMag, SwingConstants.CENTER);
+		mainPanel.add(mPlace, SwingConstants.CENTER);
 		panel.add(mainPanel, BorderLayout.CENTER);
 
-		panel.add(getValues, BorderLayout.SOUTH);
-
-		getValues.addActionListener(e -> {
-			getLargests(e);
-		});
 
 		add(panel);
 	}
 
-	public void getLargests(ActionEvent e) {
-		EarthquakeRetrofitClient earthquake = new EarthquakeRetrofitClient();
-	}
+
 
 	public static void main(String[] args) throws FileNotFoundException {
-		new EarthquakeGUI().setVisible(true);
+		EarthquakeRetrofitClient earthquake = new EarthquakeRetrofitClient();
+		new EarthquakeGUI(earthquake).setVisible(true);
 	}
 
 }
--- src/drukier/earthquake/net/EarthquakeRetrofitClient.java
+++ src/drukier/earthquake/net/EarthquakeRetrofitClient.java
@@ -13,6 +13,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EarthquakeRetrofitClient {
 
+	private String hMag;
+	private String hPlace;
+	private String dMag;
+	private String dPlace;
+	private String wMag;
+	private String wPlace;
 	private String mMag;
 	private String mPlace;
 	
@@ -23,12 +29,12 @@ public class EarthquakeRetrofitClient {
 
 		USGSEarthquakeService service = retrofit.create(USGSEarthquakeService.class);
 
-		Call<EarthquakeFeed> call = service.getAllMonth();
+		Call<EarthquakeFeed> mCall = service.getAllMonth();
 
-		call.enqueue(new Callback<EarthquakeFeed>() {
+		mCall.enqueue(new Callback<EarthquakeFeed>() {
 
 			@Override
-			public void onResponse(Call<EarthquakeFeed> call, Response<EarthquakeFeed> response) {
+			public void onResponse(Call<EarthquakeFeed> mCall, Response<EarthquakeFeed> response) {
 				EarthquakeFeed mFeed = response.body();
 
 				Optional<Earthquake> largestMonth = mFeed.getFeatures().stream()
@@ -41,12 +47,145 @@ public class EarthquakeRetrofitClient {
 			}
 
 			@Override
-			public void onFailure(Call<EarthquakeFeed> call, Throwable t) {
+			public void onFailure(Call<EarthquakeFeed> mCall, Throwable t) {
+				t.printStackTrace();
+			}
+
+		});
+		
+		Call<EarthquakeFeed> wCall = service.getAllWeek();
+
+		wCall.enqueue(new Callback<EarthquakeFeed>() {
+
+			@Override
+			public void onResponse(Call<EarthquakeFeed> wCall, Response<EarthquakeFeed> response) {
+				EarthquakeFeed mFeed = response.body();
+
+				Optional<Earthquake> largestMonth = mFeed.getFeatures().stream()
+						.max(Comparator.comparing(e -> e.getProperties().getMag()));
+
+				setmMag((String.valueOf(largestMonth.get().getProperties().getMag())));
+				setmPlace((String.valueOf(largestMonth.get().getProperties().getPlace())));
+				
+
+			}
+
+			@Override
+			public void onFailure(Call<EarthquakeFeed> wCall, Throwable t) {
+				t.printStackTrace();
+			}
+
+		});
+		
+		Call<EarthquakeFeed> dCall = service.getAllMonth();
+
+		dCall.enqueue(new Callback<EarthquakeFeed>() {
+
+			@Override
+			public void onResponse(Call<EarthquakeFeed> dCall, Response<EarthquakeFeed> response) {
+				EarthquakeFeed mFeed = response.body();
+
+				Optional<Earthquake> largestMonth = mFeed.getFeatures().stream()
+						.max(Comparator.comparing(e -> e.getProperties().getMag()));
+
+				setmMag((String.valueOf(largestMonth.get().getProperties().getMag())));
+				setmPlace((String.valueOf(largestMonth.get().getProperties().getPlace())));
+				
+
+			}
+
+			@Override
+			public void onFailure(Call<EarthquakeFeed> dCall, Throwable t) {
+				t.printStackTrace();
+			}
+
+		});
+		
+		Call<EarthquakeFeed> hCall = service.getAllWeek();
+
+		hCall.enqueue(new Callback<EarthquakeFeed>() {
+
+			@Override
+			public void onResponse(Call<EarthquakeFeed> hCall, Response<EarthquakeFeed> response) {
+				EarthquakeFeed mFeed = response.body();
+
+				Optional<Earthquake> largestMonth = mFeed.getFeatures().stream()
+						.max(Comparator.comparing(e -> e.getProperties().getMag()));
+
+				setmMag((String.valueOf(largestMonth.get().getProperties().getMag())));
+				setmPlace((String.valueOf(largestMonth.get().getProperties().getPlace())));
+				
+
+			}
+
+			@Override
+			public void onFailure(Call<EarthquakeFeed> hCall, Throwable t) {
 				t.printStackTrace();
 			}
 
 		});
 	}
+	
+
+	public String gethMag() {
+		return hMag;
+	}
+
+
+	public void sethMag(String hMag) {
+		this.hMag = hMag;
+	}
+
+
+	public String gethPlace() {
+		return hPlace;
+	}
+
+
+	public void sethPlace(String hPlace) {
+		this.hPlace = hPlace;
+	}
+
+
+	public String getdMag() {
+		return dMag;
+	}
+
+
+	public void setdMag(String dMag) {
+		this.dMag = dMag;
+	}
+
+
+	public String getdPlace() {
+		return dPlace;
+	}
+
+
+	public void setdPlace(String dPlace) {
+		this.dPlace = dPlace;
+	}
+
+
+	public String getwMag() {
+		return wMag;
+	}
+
+
+	public void setwMag(String wMag) {
+		this.wMag = wMag;
+	}
+
+
+	public String getwPlace() {
+		return wPlace;
+	}
+
+
+	public void setwPlace(String wPlace) {
+		this.wPlace = wPlace;
+	}
+
 
 	public String getmMag() {
 		return mMag;
